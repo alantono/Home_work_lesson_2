a314ec220a968468e2dcb42ade9bd71efaac0c62
Alexey <antonov1972@yandex.ru>
2022-10-17 23:50:50 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет;
+
+int a;
+
+Console.Write("Введите любое целое число ");
+int.TryParse(Console.ReadLine()!, out a);
+
+int temp = a;
+
+int d = 0; 
+while(a != 0)
+    {
+    a = a / 10; 
+    d++;
+    }
+
+Console.WriteLine($"Третья цифра введенного числа - {(temp / (int) Math.Pow(10, d - 3)) % 10}");
+
+
+
